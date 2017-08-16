9e813e4fa8c140e6ee4e31e0baf5bceecec29feb
Zach Shepherd <shepherdz@vmware.com>
2017-08-16 18:45:12 +0100

    Stub out support for enhancements
    Of the defined enhancements, only OCSP stapling seems applicable to Exim
    
    This change stubs out that functionality based on the Apache and Nginx
    plugins and leaves a TODO, based on the Exim documentation, which should
    be relatively straight-forward once we have a pattern for writing to the
    configuration file.

--- certbot-exim/certbot_exim/installer.py
+++ certbot-exim/certbot_exim/installer.py
@@ -3,6 +3,7 @@ import logging
 
 import zope.interface
 
+from certbot import errors
 from certbot import interfaces
 from certbot.plugins import common
 
@@ -24,6 +25,8 @@ class Installer(common.Plugin):
     def __init__(self, *args, **kwargs):
         super(Installer, self).__init__(*args, **kwargs)
 
+        self._enhance_func = {"staple-ocsp": self._enable_ocsp_stapling}
+
     def more_info(self):
         pass
 
@@ -39,11 +42,36 @@ class Installer(common.Plugin):
     def restart(self):
         pass
 
-    def supported_enhancements(self):
-        pass
+    def supported_enhancements(self):  # pylint: disable=no-self-use
+        """Returns currently supported enhancements."""
+        return ['staple-ocsp']
 
     def enhance(self, domain, enhancement, options=None):
-        pass
+        """Perform a configuration enhancement.
+
+        :param str domain: domain for which to provide enhancement
+        :param str enhancement: An enhancement as defined in
+            :const:`~certbot.constants.ENHANCEMENTS`
+        :param options: Flexible options parameter for enhancement.
+            Check documentation of
+            :const:`~certbot.constants.ENHANCEMENTS`
+            for expected options for each enhancement.
+
+        :raises .PluginError: If Enhancement is not supported, or if
+            an error occurs during the enhancement.
+        """
+        try:
+            return self._enhance_func[enhancement](domain, options)
+        except (KeyError, ValueError):
+            raise errors.PluginError("Unsupported enhancement: {0}".format(enhancement))
+        except errors.PluginError:
+            logger.warning("Failed %s for %s", enhancement, domain)
+            raise
+
+    def _enable_ocsp_stapling(self, domain, chain_path):
+        """Enables OCSP Stapling
+        """
+        pass  # TODO: Write `tls_ocsp_file = $chain_path` to the configuration file for $domain
 
     def save(self, title=None, temporary=False):
         pass
