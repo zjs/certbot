575ed6b0f3922fd97a9de3282f6a636a3f9105d8
Zach Shepherd <shepherdz@vmware.com>
2017-08-17 00:17:41 +0100

    Organize and document methods based on other plugins x

--- certbot-exim/certbot_exim/installer.py
+++ certbot-exim/certbot_exim/installer.py
@@ -35,7 +35,10 @@ class Installer(common.Plugin):
         return 'This plugin configures Exim to use a certificate.'
 
     def prepare(self):
-        """Check if exim daemon is running"""
+        """Check if exim daemon is installed.
+
+        :raises .errors.NoInstallationError: If Exim command cannot be found
+        """
         restart_cmd = constants.CLI_DEFAULTS['restart_cmd']
         if not util.exe_exists(restart_cmd):
             raise errors.NoInstallationError(
@@ -50,6 +53,17 @@ class Installer(common.Plugin):
 
     def deploy_cert(self, domain, cert_path, key_path, chain_path,
                     fullchain_path):
+        """Deploy certificate.
+
+        :param str domain: domain to deploy certificate file
+        :param str cert_path: absolute path to the certificate file
+        :param str key_path: absolute path to the private key file
+        :param str chain_path: absolute path to the certificate chain file
+        :param str fullchain_path: absolute path to the certificate fullchain
+            file (cert plus chain)
+
+        :raises .PluginError: when cert cannot be deployed
+        """
         pass
 
     def restart(self):
@@ -59,8 +73,24 @@ class Installer(common.Plugin):
         except errors.SubprocessError as err:
             raise errors.MisconfigurationError(str(err))
 
+    def config_test(self):
+        """Check the Exim configuration for errors.
+
+        :raises .errors.MisconfigurationError: If config_test fails
+        """
+        pass
+
+    ##################################
+    # Enhancement methods (IInstaller)
+    ##################################
+
     def supported_enhancements(self):  # pylint: disable=no-self-use
-        """Returns currently supported enhancements."""
+        """Returns a `collections.Iterable` of supported enhancements.
+
+        :returns: supported enhancements which should be a subset of
+            :const:`~certbot.constants.ENHANCEMENTS`
+        :rtype: :class:`collections.Iterable` of :class:`str`
+        """
         return ['staple-ocsp']
 
     def enhance(self, domain, enhancement, options=None):
@@ -87,20 +117,68 @@ class Installer(common.Plugin):
 
     def _enable_ocsp_stapling(self, domain, chain_path):
         """Enables OCSP Stapling
+
+        :param str domain: domain to enable OCSP response for
+        :param chain_path: chain file path
+        :type chain_path: `str` or `None`
         """
         pass  # TODO: Write `tls_ocsp_file = $chain_path` to the configuration file for $domain
 
+    ###################################################
+    # Wrapper functions for Reverter class (IInstaller)
+    ###################################################
+
+    # TODO: Implement these methods using a `reverter.Reverter` instance
+
     def save(self, title=None, temporary=False):
+        """Saves all changes to the configuration files.
+
+        Both title and temporary are needed because a save may be
+        intended to be permanent, but the save is not ready to be a full
+        checkpoint.
+
+        It is assumed that at most one checkpoint is finalized by this
+        method. Additionally, if an exception is raised, it is assumed a
+        new checkpoint was not finalized.
+
+        :param str title: The title of the save. If a title is given, the
+            configuration will be saved as a new checkpoint and put in a
+            timestamped directory. `title` has no effect if temporary is true.
+
+        :param bool temporary: Indicates whether the changes made will
+            be quickly reversed in the future (challenges)
+
+        :raises .errors.PluginError: If there was an error in
+            an attempt to save the configuration, or an error creating a
+            checkpoint
+        """
         pass
 
     def rollback_checkpoints(self, rollback=1):
+        """Revert `rollback` number of configuration checkpoints.
+
+        :param int rollback: Number of checkpoints to revert
+
+        :raises .errors.PluginError: If there is a problem with the input or
+            the function is unable to correctly revert the configuration
+        """
         pass
 
     def recovery_routine(self):
+        """Revert configuration to most recent finalized checkpoint.
+
+        Remove all changes (temporary and permanent) that have not been
+        finalized. This is useful to protect against crashes and other
+        execution interruptions.
+
+        :raises .errors.PluginError: If unable to recover the configuration
+        """
         pass
 
     def view_config_changes(self):
-        pass
+        """Show all of the configuration changes that have taken place.
 
-    def config_test(self):
+        :raises .errors.PluginError: If there is a problem while processing
+            the checkpoints directories.
+        """
         pass
