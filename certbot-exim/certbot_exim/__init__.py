356aa99d201cba652eb87fac104e809f2843af74
Zach Shepherd <shepherdz@vmware.com>
2017-08-17 09:20:26 +0100

    Attempt to document the plugin based on current implementation.
    This will likely evolve as the implementation progresses, but is a
    starting point for documentation.

--- certbot-exim/certbot_exim/__init__.py
+++ certbot-exim/certbot_exim/__init__.py
@@ -1,18 +1,62 @@
 """
-The `~certbot_exim.installer` plugin automates the process of installing
-certificates for the Exim mail transfer agent.
+The `~certbot_exim.installer` plugin automates the process of installing certificates for the Exim
+mail transfer agent.
+
+The plugin will configure Exim to use the certificate and private key managed by Certbot.
+
+The plugin will enable STARTTLS and set required ciphers based on Mozilla recommendations.
 
 
 Named Arguments
 ---------------
 
-TODO
+=============================  ==========================================================
+``--exim-server-root``         The Exim server's root directory (Default: ``/etc/exim4``)
+``--exim-file-configuration``  ``single-file`` to indicate a single file is used for
+                               configuration or ``split-file`` to indicate that a
+                               configuration directory is used.
+=============================  ==========================================================
+
+
+Supported Enhancements
+----------------------
+
+=============================  ==========================================================
+``--staple-ocsp``              Enables OCSP Stapling. A valid OCSP response is
+                               stapled to the certificate that the server offers
+                               during TLS. (default: None)
+=============================  ==========================================================
 
 
 Examples
 --------
 
-TODO
+.. code-block:: bash
+   :caption: To acquire a certificate for ``example.com`` using the ``standalone`` plugin to
+             complete a ``http-01`` challenge, configure Exim to use it, and enable OCSP stapling.
 
+   certbot --exim \\
+     --standalone \\
+     --staple-ocsp \\
+     -d example.com
 
+.. code-block:: bash
+   :caption: To acquire a certificate for ``example.com`` using the ``standalone`` plugin to
+             complete a ``http-01`` challenge and configure Exim to use it following the split-file
+            pattern, without enabling OCSP stapling.
+
+   certbot --exim \\
+     --standalone \\
+     --exim-file-configuration=split-file \\
+     -d example.com
+
+.. code-block:: bash
+   :caption: To acquire a certificate for ``example.com`` using the ``dns-cloudflare`` plugin to
+             complete a ``dns-01`` challenge and configure Exim to use it, without enabling OCSP
+             stapling.
+
+   certbot --exim \\
+     --dns-cloudflare \\
+     --dns-cloudflare-credentials ~/.secrets/certbot/cloudflare.ini \\
+     -d example.com
 """
